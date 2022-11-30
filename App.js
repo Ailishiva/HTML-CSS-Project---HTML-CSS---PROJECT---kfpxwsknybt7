3ddbe4ce1ffd06757720c797bb9a74349802d413
Aili shivaprasad <aili.shivaprasad@gmail.com>
2022-11-30 09:25:58 +0530

    commited

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
+function submit(){
+    location.replace("Work.jpg");
 
+}
+
+function replace(){
+    location.replace("blog.jpg");
+}
+
+function about(){
+    location.replace("About us.jpg");
+}
