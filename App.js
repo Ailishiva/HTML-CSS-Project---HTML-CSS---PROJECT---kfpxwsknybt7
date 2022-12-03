4b86258ddb7a31197a093bd954c300cd2bc7d4f7
Aili shivaprasad <aili.shivaprasad@gmail.com>
2022-12-03 15:33:53 +0530

    comiited

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-function submit(){
-    location.replace("Work.jpg");
 
+let content = document.querySelectorAll('.content');
+let icons = document.querySelectorAll('nav ul li');
+
+icons.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        hideAllImages();
+        hideAllIcons();
+
+        item.classList.add('active');
+        content[idx].classList.add('show');
+        
+    })
+})
+
+function hideAllImages() {
+     content.forEach((img) => {
+        img.classList.remove('show');
+    })
 }
 
-function replace(){
-    location.replace("blog.jpg");
-}
-
-function about(){
-    location.replace("About us.jpg");
+function hideAllIcons() {
+    icons.forEach((item) => {
+        item.classList.remove('active');
+    })
 }
